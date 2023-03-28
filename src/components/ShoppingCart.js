cac9c0e896d67c74b022cdacc4e37f6d7e14a210
Anerazora <arozarenajesus@gmail.com>
2023-03-29 01:13:28 +0200

    Calculo precio total del pedido

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../contexts/ShoppingCartContext";
 
 export const ShoppingCart = () => {
-      return (
-        <div>ShoppingCart component</div>
-      )
-    }
+  const [cart, setCart] = useContext(CartContext);
+
+  const quantity = cart.reduce((acc, curr) => {
+    return acc + curr.quantity;
+  }, 0);
+
+  const totalPrice = cart.reduce(
+    (acc, curr) => acc + curr.quantity * curr.price,
+    0
+  );
+
+  return (
+    <div className="cart-container">
+      <div>
+        <div>Items in cart: {quantity}</div>
+        <div>Total: ${totalPrice}</div>
+        <button onClick={() => console.log(cart)}>Checkout</button>
+      </div>
+    </div>
+  );
+};
