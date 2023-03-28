1f47d75ae5b6318bd2f5914bde312b9da32b8784
Anerazora <arozarenajesus@gmail.com>
2023-03-29 00:27:43 +0200

    Doy forma al carrito

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import { ItemList } from "./components/ItemList";
 
 import { Navbar } from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ShoppingCart } from "./components/ShoppingCart";
 
 export const App = () => {
   return (
     <div>
-      <Navbar />
-      <ItemList />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ItemList />}/>
+          <Route path="/cart" element={<ShoppingCart />}/>
+        </Routes>
+
+      </Router>
     </div>
   );
 };
