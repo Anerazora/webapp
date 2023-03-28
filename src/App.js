d2632c8ed1b2e828e35e46394b353433cf7087c4
Anerazora <arozarenajesus@gmail.com>
2023-03-29 00:51:04 +0200

    Funciones añadir y quitar productos del carrito

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import { ItemList } from "./components/ItemList";
 import { Navbar } from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ShoppingCart } from "./components/ShoppingCart";
+import { ShoppingCartProvider } from "./contexts/ShoppingCartContext";
 
 export const App = () => {
   return (
-    <div>
+    <ShoppingCartProvider>
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<ItemList />}/>
-          <Route path="/cart" element={<ShoppingCart />}/>
+          <Route path="/" element={<ItemList />} />
+          <Route path="/cart" element={<ShoppingCart />} />
         </Routes>
-
       </Router>
-    </div>
+    </ShoppingCartProvider>
   );
 };
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../contexts/ShoppingCartContext";
 
 export const Item = ({ name, price, id, imgUrl }) => {
+    const [cart, setCart] = useContext(CartContext);
+    const addToCart = () => {
+        setCart((currItems) => {
+          const isItemsFound = currItems.find((item) => item.id === id);
+          if (isItemsFound) {
+            return currItems.map((item) => {
+              if (item.id === id) {
+                return { ...item, quantity: item.quantity + 1 };
+              } else {
+                return item;
+              }
+            });
+          } else {
+            return [...currItems, { id, quantity: 1, price }];
+          }
+        });
+      };
+      
     return (
         <div className="item-box">
             <div>{name}</div>
