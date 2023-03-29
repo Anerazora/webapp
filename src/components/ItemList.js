1ecc46e4d0eb856edebc2ebe5a67b586c8c8aacf
Anerazora <arozarenajesus@gmail.com>
2023-03-29 23:34:09 +0200

    Traigo datos de Realtime Database y Storage

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,11 +1,37 @@
 import React from "react";
-import storeItems from "../data/products.json";
+//import storeItems from "../data/products.json";
 import { Item } from "./Item";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export const ItemList = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+      //console.log('Se monta Productos');
+      axios.get('https://webapp-96011-default-rtdb.europe-west1.firebasedatabase.app/products.json')
+          .then((response) => {
+              //console.log(response.data);
+              let arrayProducts = [];
+              for (let key in response.data) {
+                  arrayProducts.push({
+                      id: key,
+                      name: response.data[key].name,
+                      price: response.data[key].price,
+                      imgUrl: response.data[key].imgUrl
+                  })
+              }
+              
+              setProducts(arrayProducts);
+          }).catch((error)=>{
+              alert('Se ha producido un error');
+          })
+  },[]);
+
   return (
     <div className="items-list">
-      {storeItems.map((product, idx) => {
+      {products.map((product, idx) => {
         return <Item key={product.id} {...product} />;
       })}
     </div>
